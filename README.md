a7020ae279d4fb7e9bfe3be28cdf6cbe9386a320
Urdex-finance <contact@urdex.finance>
2023-03-31 14:31:37 +0700

    urd token

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Urdex-contract
+# Urdex-finance
+
+A community-driven project that aims to solve the liquidity problem by enabling disparate forms of liquidity to connect with markets in a decentralized way.
---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+  },
+};
+
+export default config;
